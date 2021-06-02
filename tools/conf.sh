b6d3800dcfa544d3d83e77d768bcbd4693609469
sherlockwu <wukanustc@gmail.com>
2021-06-02 16:30:02 -0500

    fix lib path for libjemalloc

--- tools/conf.sh
+++ tools/conf.sh
@@ -1,4 +1,6 @@
 cd /etc/ld.so.conf.d
-echo "/users/scarletg/Sundial/libs/" | sudo tee -a other.conf
+#echo "/users/scarletg/Sundial/libs/" | sudo tee -a other.conf
+echo "/users/kanwu/Sundial/libs/" | sudo tee -a other.conf
 echo "/usr/local/lib" | sudo tee -a other.conf
+echo "/usr/lib/x86_64-linux-gnu/" | sudo tee -a other.conf
 sudo /sbin/ldconfig
