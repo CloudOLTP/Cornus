878c854a595da16d3231cd71708b8cdfa7cd7d16
sherlockwu <wukanustc@gmail.com>
2021-08-17 12:55:00 +0000

    sync

--- setup.sh
+++ setup.sh
@@ -1,7 +1,6 @@
 cd tools/
 ./setup_grpc.sh
 ./setup_redis.sh
-./setup_stunnel.sh
 ./conf.sh
 cd ..
 ./run_proto.sh
--- sync.sh
+++ sync.sh
@@ -1,3 +1,7 @@
 rsync -av --exclude 'proto' --delete /home/kanwu/Sundial/ kanwu@compute2:/home/kanwu/Sundial/
 rsync -av --exclude 'proto' --delete /home/kanwu/Sundial/ kanwu@compute3:/home/kanwu/Sundial/
 rsync -av --exclude 'proto' --delete /home/kanwu/Sundial/ kanwu@compute4:/home/kanwu/Sundial/
+rsync -av --exclude 'proto' --delete /home/kanwu/Sundial/ kanwu@compute5:/home/kanwu/Sundial/
+rsync -av --exclude 'proto' --delete /home/kanwu/Sundial/ kanwu@compute6:/home/kanwu/Sundial/
+rsync -av --exclude 'proto' --delete /home/kanwu/Sundial/ kanwu@compute7:/home/kanwu/Sundial/
+rsync -av --exclude 'proto' --delete /home/kanwu/Sundial/ kanwu@compute8:/home/kanwu/Sundial/
