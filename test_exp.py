000588a2df9cbb5904d021140b0474de7c53eb3d
ScarletGuo <zguo74@wisc.edu>
2021-08-29 11:19:01 -0500

    dont collect stats if failure node

--- test_exp.py
+++ test_exp.py
@@ -11,66 +11,69 @@ from test import parse_arg, try_compile, collect_result, load_job, eval_arg
 script = "test_distrib.py"
 
 if __name__ == "__main__":
-    job = load_job(sys.argv[1:])
-    assert("CONFIG" in job) 
-    exp_name = job["CONFIG"].split('/')[-1].split('.')[0]
-    if "NODE_ID" in job: 
-        curr_node = int(job["NODE_ID"])
-    else:
-        curr_node = 0 
-    args = [""]
-    for key in job:
-        new_args = []
-        if key == "CONFIG":
-            continue
-        if isinstance(job[key], list):
-            for i, x in enumerate(job[key]):
-                for arg in args:
-                    arg = arg + "{}={} ".format(key, x)
-                    new_args.append(arg)
-        else:
-            for arg in args:
-                arg = arg + "{}={} ".format(key, job[key])
-                new_args.append(arg)
-        args = new_args
-    for i, arg in enumerate(args):
-        print("[LOG] issue exp {}/{}".format(i+1, len(args)))
-        print("[LOG] arg = {}".format(arg), flush=True)
-        if script == "test_distrib.py":
-            ret = start_nodes(arg.split(), curr_node)
-            if ret != 0:
-                continue
-            print("[LOG] KILLING CURRENT SERVER ... ")
-            # kill the remote servers
-            kill_nodes(curr_node)
-            print("[LOG] FINISH EXECUTION ", flush=True)
-        else:
-            main(arg)
-    if eval_arg("MODE", "release", job, default=True):
-        os.system("cd outputs/; python3 collect_stats.py; mv stats.csv {}.csv; mv stats.json {}.json".format(exp_name, exp_name))
-    print("[LOG] FINISH WHOLE EXPERIMENTS")
-    print("[LOG] Start collecting results from remote", flush=True)
-    f = open('ifconfig.txt')
-    num_nodes = 0
-    for addr in f:
-        if num_nodes == job["NUM_NODES"]:
-            print("[LOG] num_nodes == job[\"NUM_NODES\"]. Stop ssh-ing remote.")
-            break
-        if '#' in addr:
-            continue
-        if "=l" in addr:
-            print("[LOG] end of ifconfig.txt reached. Stop ssh-ing remote.")
-            break
-        if curr_node == num_nodes:
-            num_nodes += 1
-            continue
-        # start server
-        addr = addr.split(':')[0]
-        os.system("ssh {} 'cd Sundial/outputs/; python3 collect_stats.py; mv stats.csv {}.csv; mv stats.json {}.json'".format(addr, exp_name, exp_name))
-        num_nodes += 1
-    suffix = ""
-    if eval_arg("FAILURE_ENABLE", "true", job, default=False):
-        suffix = " {}".format(job["FAILURE_NODE"])
-    os.system("cd tools; python3 collect_remote_result.py {} {}".format(exp_name, num_nodes) + suffix)
-    print("[LOG] FINISH collecting results")
-    #os.system("python3 send_email.py {}".format(exp_name))
+	job = load_job(sys.argv[1:])
+	assert("CONFIG" in job) 
+	exp_name = job["CONFIG"].split('/')[-1].split('.')[0]
+	if "NODE_ID" in job: 
+		curr_node = int(job["NODE_ID"])
+	else:
+		curr_node = 0 
+	args = [""]
+	for key in job:
+		new_args = []
+		if key == "CONFIG":
+			continue
+		if isinstance(job[key], list):
+			for i, x in enumerate(job[key]):
+				for arg in args:
+					arg = arg + "{}={} ".format(key, x)
+					new_args.append(arg)
+		else:
+			for arg in args:
+				arg = arg + "{}={} ".format(key, job[key])
+				new_args.append(arg)
+		args = new_args
+	for i, arg in enumerate(args):
+		print("[LOG] issue exp {}/{}".format(i+1, len(args)))
+		print("[LOG] arg = {}".format(arg), flush=True)
+		if script == "test_distrib.py":
+			ret = start_nodes(arg.split(), curr_node)
+			if ret != 0:
+				continue
+			print("[LOG] KILLING CURRENT SERVER ... ")
+			# kill the remote servers
+			kill_nodes(curr_node)
+			print("[LOG] FINISH EXECUTION ", flush=True)
+		else:
+			main(arg)
+	if eval_arg("MODE", "release", job, default=True):
+		os.system("cd outputs/; python3 collect_stats.py; mv stats.csv {}.csv; mv stats.json {}.json".format(exp_name, exp_name))
+	print("[LOG] FINISH WHOLE EXPERIMENTS")
+	print("[LOG] Start collecting results from remote", flush=True)
+	f = open('ifconfig.txt')
+	num_nodes = 0
+	for addr in f:
+		if num_nodes == job["NUM_NODES"]:
+			print("[LOG] num_nodes == job[\"NUM_NODES\"]. Stop ssh-ing remote.")
+			break
+		if '#' in addr:
+			continue
+		if "=l" in addr:
+			print("[LOG] end of ifconfig.txt reached. Stop ssh-ing remote.")
+			break
+		if curr_node == num_nodes:
+			num_nodes += 1
+			continue
+		if eval_arg("FAILURE_ENABLE", "true", job, default=False) and curr_node == job["FAILURE_NODE"]:
+			num_nodes += 1
+			continue
+		# collect result on each server if not a failure node
+		addr = addr.split(':')[0]
+		os.system("ssh {} 'cd Sundial/outputs/; python3 collect_stats.py; mv stats.csv {}.csv; mv stats.json {}.json'".format(addr, exp_name, exp_name))
+		num_nodes += 1
+	suffix = ""
+	if eval_arg("FAILURE_ENABLE", "true", job, default=False):
+		suffix = " {}".format(job["FAILURE_NODE"])
+	os.system("cd tools; python3 collect_remote_result.py {} {}".format(exp_name, num_nodes) + suffix)
+	print("[LOG] FINISH collecting results")
+	#os.system("python3 send_email.py {}".format(exp_name))
