319a0f8100ecc70b39699293fe128e5c56bacd3c
sherlockwu <wukanustc@gmail.com>
2021-08-31 21:26:43 +0000

    updated experiments configs

--- experiments/run_all_exp.sh
+++
@@ -1,7 +0,0 @@
-cd ..
-list=("ycsb_perc_remote" "ycsb_read_ratio" "ycsb_zipf" "ycsb_log_delay" "tpcc_wh")
-for value in ${list[@]}
-do
-  python3 test_exp.py experiments/$value.json 0
-  sleep 5
-done
--- run_exp.sh
+++ run_exp.sh
@@ -1,8 +1,10 @@
-# python3 test_exp.py CONFIG=experiments/ycsb_failure.json MODE=release
-# python3 test_exp.py CONFIG=experiments/azure-redis/num_nodes_blob_0828.json MODE=release &> log/num_nodes_blob_0828.log ;
-# python3 test_exp.py CONFIG=experiments/azure-redis/num_nodes_redis_0828.json MODE=release &> log/num_nodes_redis_0828.log
-python3 test_exp.py CONFIG=experiments/azure-redis/failure_zipf_redis_0829.json MODE=release &> log/failure_zipf_redis_0829.log
-python3 test_exp.py CONFIG=experiments/azure-redis/failure_zipf_blob_0829.json MODE=release &> log/failure_zipf_blob_0829.log
-python3 test_exp.py CONFIG=experiments/azure-redis/failure_num_nodes_blob_0829.json MODE=release &> log/failure_num_nodes_blob_0829.log
-python3 test_exp.py CONFIG=experiments/azure-redis/failure_num_nodes_redis_0829.json MODE=release &> log/failure_num_nodes_redis_0829.log
+for exp in failure_num_nodes num_nodes zipf read_perc 
+do
+for st in blob iso_blob
+do
+	exp_name="${exp}_${st}"
+	echo ${exp_name}
+	python3 test_exp.py CONFIG=experiments/azure-redis/${exp_name}.json MODE=release AZURE_ISOLATION_ENABLE=false &> log/${exp_name}.log ;
+done
+done
 
