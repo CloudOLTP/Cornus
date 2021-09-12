343f8c65b4a6d309c222eaf5ee21079d0cfaf7e3
sherlockwu <wukanustc@gmail.com>
2021-09-12 03:08:59 +0000

    redis_rerun

--- run_exp.sh
+++ run_exp.sh
@@ -1,6 +1,6 @@
 for exp in zipf #read_perc zipf num_nodes failure_num_nodes
 do
-for st in redis_repeated #iso_blob blob 
+for st in redis_repeated #blob_iso blob 
 do
 	exp_name="${exp}_${st}"
 	echo ${exp_name}
