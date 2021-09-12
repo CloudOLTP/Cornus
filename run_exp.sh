ec177379ab8d225074324c6332aaa216aafa5b4a
sherlockwu <wukanustc@gmail.com>
2021-09-12 00:15:58 +0000

    blob-iso

--- run_exp.sh
+++ run_exp.sh
@@ -1,10 +1,11 @@
-for exp in num_nodes read_perc zipf failure_num_nodes
+for exp in zipf #read_perc zipf num_nodes failure_num_nodes
 do
-for st in blob iso_blob
+for st in redis_repeated #iso_blob blob 
 do
 	exp_name="${exp}_${st}"
 	echo ${exp_name}
-	python3 test_exp.py CONFIG=experiments/azure-redis/${exp_name}.json MODE=release AZURE_ISOLATION_ENABLE=false &> log/${exp_name}.log ;
+	ls experiments/azure-redis/${exp_name}*
+	python3 test_exp.py CONFIG=experiments/azure-redis/${exp_name}.json MODE=release &> log/${exp_name}.log ;
 done
 done
 
